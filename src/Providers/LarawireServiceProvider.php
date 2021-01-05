8e234d58e79c0fce5c5586eefec7dc079c799fe4
redbastie <redbastie@icloud.com>
2021-01-05 08:24:27 -0500

    resolve relations

--- src/Providers/LarawireServiceProvider.php
+++ src/Providers/LarawireServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace Redbastie\Larawire\Providers;
 
-use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\ServiceProvider;
 use Redbastie\Larawire\Commands\MakeAuthCommand;
 use Redbastie\Larawire\Commands\MakeCrudCommand;
@@ -21,21 +20,5 @@ class LarawireServiceProvider extends ServiceProvider
         }
 
         $this->loadRoutesFrom(__DIR__ . '/../../routes/web.php');
-        $this->resolveRelationships();
-    }
-
-    public function resolveRelationships()
-    {
-        foreach ((new Filesystem)->allFiles(app_path('Models')) as $file) {
-            $class = app('App\\Models\\' . str_replace(['/', '.php'], ['\\', ''], $file->getRelativePathname()));
-
-            if (method_exists($class, 'relationships')) {
-                foreach ($class->relationships() as $key => $relationship) {
-                    $class->resolveRelationUsing($key, function () use ($class, $key) {
-                        return $class->relationships()[$key];
-                    });
-                }
-            }
-        }
     }
 }
