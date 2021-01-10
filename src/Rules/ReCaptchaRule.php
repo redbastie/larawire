b2d7bdea63cc88906b21d72d57b92c9a1b487a5f
redbastie <redbastie@icloud.com>
2021-01-10 14:18:38 -0500

    added recaptcha

---
+++ src/Rules/ReCaptchaRule.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Redbastie\Larawire\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+use ReCaptcha\ReCaptcha;
+
+class ReCaptchaRule implements Rule
+{
+    public function passes($attribute, $value)
+    {
+        $recaptcha = new ReCaptcha(config('services.recaptcha.secret_key'));
+        $resp = $recaptcha->verify($value, request()->ip());
+
+        return $resp->isSuccess();
+    }
+
+    public function message()
+    {
+        return trans('The reCAPTCHA response is invalid.');
+    }
+}
